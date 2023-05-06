cc9023e6fad9dd054a0b13832fd235965f3606f1
ChristianJames Luces <christianjames231103@gmail.com>
2023-05-06 21:47:10 +0800

    ask the user for an operation

--- calculator.py
+++ calculator.py
@@ -8,7 +8,11 @@ print(intro)
 print("\033[44;1m" + "This program is a simple app calculator that will ask the user to a math operation and perform it." + "\033[0m")
 input("Press the ENTER key to run the program....\n")
 time.sleep(5)
-#Ask the user to choose one operation
+#Create a loop with True condition
+while True:
+    #Ask the user to choose one operation
+    operation = str(input("Choose one of the four math operatoins (Addition, Subtraction, Multiplication, and Division)"))
+    
 #Ask the user for two numbers
 #Perform the operation base from the user's choiced
 #Display the result
