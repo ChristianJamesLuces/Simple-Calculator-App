e08bd56e6a9d501d4c9a986cc6a3b552cce8c4f5
ChristianJames Luces <christianjames231103@gmail.com>
2023-05-06 21:32:23 +0800

    Pseudocode

---
+++ calculator.py
@@ -0,0 +1,9 @@
+#Pseudocode
+#Import the necessary modules
+#Welcome message and its function
+#Ask the user to choose one operation
+#Ask the user for two numbers
+#Perform the operation base from the user's choiced
+#Display the result
+#Ask the user if they want to try again
+#Display "Thank you!" before it exits
